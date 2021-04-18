3b692474e247580a04f65decee7e944f5ae6c5f7
Prerna <namr2501@gmail.com>
2021-04-18 08:53:22 +0530

    3rd file to github

---
+++ string_in_pyton.py
@@ -0,0 +1,26 @@
+print("""Hey!!
+I just used
+a multiline
+print statement""")
+
+print("Next part is the use of \\n")
+print("I\'ve used \n \\n here!")
+
+st="Hey.."
+st2='I\'m your friend'
+
+print("st1="+st+"\n"+"st2="+st2)
+print(st+" "+st2)
+print("I concatenated both the string.")
+
+print(""" How does it feel to
+Multiply a string ?
+ Look here :""")
+print(st*3)
+
+print("It\'s so interesting!! Hehe")
+print(4*2)
+print(4*'2')
+print('4'*2)
+print("WOOOWWWW!!")
+print("\n That's all for today in string for now.\n see you at next topic!")
