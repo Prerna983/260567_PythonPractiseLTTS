a1f7388c9467132fb8bd99fd089512406b7a1f48
Prerna <namr2501@gmail.com>
2021-04-18 18:07:47 +0530

    5th file to github

---
+++ boolean_&_comparision.py
@@ -0,0 +1,15 @@
+new=True
+print(new)
+
+print("hi" == "hello")
+print(("hello" == "hello"))
+print(("Hello" == "hello"))
+
+print(1==4)
+print((1==1) and 2==2)
+print (1!=4)
+print(10>5)
+print(9 == 9.000)
+print((8.5>8))
+print("apple">"ant")
+print("PARSE"=="parse")
