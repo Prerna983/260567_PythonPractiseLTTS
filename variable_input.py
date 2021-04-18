66efce9ae2d45d2191953083bf5c88e043cde99d
Prerna <namr2501@gmail.com>
2021-04-18 16:06:30 +0530

    input operations on file

---
+++ variable_input.py
@@ -0,0 +1,18 @@
+name=input("Enter name: ")
+print("Welcome "+name+"!")
+
+age=(input("Enter age: "))
+print(name+" is "+age+" years old.")
+
+x=int(input("x="))
+y=int(input("y="))
+sum=x+y
+print("sum= "+str(sum))
+
+str1="spam"
+str1+=" eggs"
+print(str1)
+
+t="ab"
+t*=3
+print((t))
