3a6510a282788862170536cdee3588b68ec27656
Prerna <namr2501@gmail.com>
2021-04-19 09:33:41 +0530

    7th file to github

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
 print("Hello World!!")
+
+print("Hello","World","!!",sep="-")
+
+print("She","sells","sea","shells","on","the","sea","shore",sep="  ")
+
+print("She","sells","sea","shells",end=" ")
+print("on","the","sea","shore")
+
+print("\nShe","sells","sea","shells",end="-----")
+print("on","the","sea","shore")
+
+print("Hello","World","!!",sep="--!--",end="........\n")
+
+print("    *    "*2,"   * *   "*2,"  *   *  "*2," *     * "*2,"***   ***"*2,"  *   *  "*2,"  *   *  "*2,"  *****  "*2,sep="\n")
