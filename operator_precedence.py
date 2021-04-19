7060f2e2f0a151582cb0571d325d3f650c87aa25
Prerna <namr2501@gmail.com>
2021-04-19 18:04:13 +0530

    8th file

---
+++ operator_precedence.py
@@ -0,0 +1,16 @@
+print(2*3%5)
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+
+print(9%3%2)
+print(8%3%4)
+print(("The exponential has right sided binding"))
+print((2**3)**2)
+print(2**(3**2))
+print((2**3**2))
+
+print((2 ** 4), (2 * 4.), (2 * 4))
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+
+print("\n**\n*,/,//,%\n+,-")
