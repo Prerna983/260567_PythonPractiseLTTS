7e71a87de699e92cd5832dbd4c0f2a0b753ca4bf
Prerna <namr2501@gmail.com>
2021-04-19 18:12:31 +0530

    9th file

---
+++ comments.py
@@ -0,0 +1,22 @@
+# This program evaluates the hypotenuse c.
+# a and b are the lengths of the legs.
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5  # We use ** instead of square root.
+print("c =", c)
+
+# This is a test program.
+x = 1
+y = 2
+# y = y + x
+print(x + y)
+
+print("I've also learnt multi-line comments")
+# to make a multi line
+# comment
+# select all the
+# lines you want to
+# comment
+# and then press
+# ctrl+/
+# same goes for de-comment
