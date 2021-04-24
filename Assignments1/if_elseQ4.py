e1002ee27d94c88310063f835de763102c5d4a77
Prerna <namr2501@gmail.com>
2021-04-24 22:54:22 +0530

    commit

---
+++ Assignments1/if_elseQ4.py
@@ -0,0 +1,9 @@
+# Write a python program to check if the input number is
+# -real number
+# -float numner
+# -string-
+# complex number
+# -Zero (0)
+
+x=eval(input("Enter a number: "))
+print(type(x))
