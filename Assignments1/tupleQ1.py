816a9d3112249565e1b7041d9796e5909ed5c24f
Prerna <namr2501@gmail.com>
2021-04-24 15:26:55 +0530

    commit

---
+++ Assignments1/tupleQ1.py
@@ -0,0 +1,13 @@
+# Write a Python program to find the repeated items of a tuple
+
+l = tuple(map(int, input("Enter tuple: ").split(" ")))
+l1 = list(set(l))
+
+for i in l1:
+    c = 0
+    for j in l:
+
+        if i == j:
+            c = c + 1
+    if c > 1:
+        print(i)
