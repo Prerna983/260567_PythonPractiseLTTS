aef00629a424b0750f1e2f650cbf0c8506e773f9
Prerna <namr2501@gmail.com>
2021-04-24 15:14:41 +0530

    commits

--- Assignment/ListQ1.py
+++ Assignment/ListQ1.py
@@ -1,5 +1,5 @@
 # Write a Python program to find the second smallest number in a list
 
-l=list(map(int,input("enter no:").split(" ")))
+l=list(map(int,input("enter list: ").split(" ")))
 l.sort()
 print(l[1])
