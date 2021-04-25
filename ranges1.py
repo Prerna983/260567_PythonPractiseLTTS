c91e428af30b060be9d1cfb271316b4296aabaee
Prerna <namr2501@gmail.com>
2021-04-25 08:53:01 +0530

    commit

---
+++ ranges1.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print(i,end=" ")    #0 to 9
+
+print()
+for i in range(5,11):
+    print(i,end=" ")    #5,6,7,8,9,10
+
+print()
+for i in range(3,30,3):
+    print(i,end=" ")    #multiples of 3 from 3 to 27
+
+print()
+for i in range(40,1,-4):
+    print(i,end=" ")    #multiples of 4 from 40 to 4
